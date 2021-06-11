4a74beb448e2330abfd0a17d147dfb225302af71
pointblank <pointblank.connect@gmail.com>
2021-06-11 12:36:45 -0700

    'init'

---
+++ src/views/About.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="full-wrapper">
+    
+  
+<jj-parallax-card class="" lightColor="transparent" style="margin:auto; width: 512px; height: 512px;" @click="click">
+      <div slot="front">
+         <vh-copy class="para-desktop copy-button"
+      :data="'0x1e1eed62f8d82ecfd8230b8d283d5b5c1ba81b55'"
+      :confirm_dialog="'buefy'"
+      @copied="copiedData">
+      Contract Address
+</vh-copy><br>
+ <h1>GAMMA tokens correspond to the luminance value of LED pixels.</h1>
+<br><p>
+
+Present on the exposed surfaces and livestreamed from a satellite to be placed in LEO in JUNE 2022, These pixel's hue (KAPPA), luminosity (GAMMA), X-Axis (BETA) and Y-Axis (RHO) locations are acquired in exchange for the aforementioned tokens.</p>    <br>    <router-link :to="{ name: 'home' }" >
+     <img src="~/@/assets/gamma-26.png" style="height:64px;filter:invert(1);" alt="">
+    </router-link></div>
+</jj-parallax-card>
+
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+export default {
+  data() {
+    return {
+      form: {
+        quantity: ''
+      },
+      modalLoginOpen: false,
+      modalMakepotionOpen: false
+    };
+  },
+ components: {
+  },
+};
+</script>
+<style scoped>
+.hasEffect {
+  cursor: pointer;
+}
+</style>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="full-wrapper">
+ 
+
+<div class="about-wrapper">
+  <div class="about-text">
+       <router-link :to="{ name: 'about' }" >
+      <h1 style="color:white;">GAMMA PROTOCOL <img src="~/@/assets/paper.png" style="filter:invert(1);" alt="">
+      </h1>
+    </router-link>
+    </div>
+
+    <div class="logo-center-wrapper" style="">
+<div class="siblings greek-icons">
+  <a target="_blank" href="https://xiprotocol.io">
+<img src="~/@/assets/xi_token_icon_v0.1-48.png" alt="" class="sibling-icon">
+</a>
+  <a target="_blank" href="https://betaprotocol.io">
+<img src="~/@/assets/beta-13.png" alt="" class="middle-1 sibling-icon">
+</a>
+  <a target="_blank" href="https://kappaprotocol.io">
+<img src="~/@/assets/kappa-36.png" alt="" class="middle-2 sibling-icon">
+</a>
+  <a target="_blank" href="https://rhoprotocol.io">
+<img src="~/@/assets/rho_icon-06.png" alt="" class="sibling-icon">
+</a>
+</div>
+
+      <vh-copy class="para-desktop copy-button"
+      :data="'0x1e1eed62f8d82ecfd8230b8d283d5b5c1ba81b55'"
+      :confirm_dialog="'buefy'"
+      @copied="copiedData">
+
+<jj-parallax-card class="" lightColor="transparent" style="filter:invert(1);margin:auto; width: 512px; height: 512px;" @click="click">
+      <div slot="front">
+      <img src="~/@/assets/gamma-26.png" width="512px;" height="512px;" class="img-top" alt="">
+      </div>
+</jj-parallax-card>
+
+      </vh-copy>
+
+      <vh-copy class="para-mobile copy-button"
+      :data="'0x1e1eed62f8d82ecfd8230b8d283d5b5c1ba81b55'"
+      :confirm_dialog="'buefy'"
+      @copied="copiedData">
+
+<jj-parallax-card class="" lightColor="transparent" style="filter:invert(1);margin:auto; width: 300px; height: 300px;" @click="click">
+      <div slot="front">
+      <img src="~/@/assets/gamma-26.png" width="300px;" height="300px;" class="img-top" alt="">
+      </div>
+</jj-parallax-card>
+
+      </vh-copy>
+ 
+<div class="siblings">
+  <a target="_blank" href="https://app.uniswap.org/#/swap?outputCurrency=0x1e1eed62f8d82ecfd8230b8d283d5b5c1ba81b55">
+<img src="~/@/assets/uniswap.png" alt="" class="sibling-icon">
+</a>
+  <a target="_blank" href="https://etherscan.io/address/0x1e1eed62f8d82ecfd8230b8d283d5b5c1ba81b55">
+<img src="~/@/assets/etherscan.png" alt="" class="middle-3 sibling-icon">
+</a>
+  <a target="_blank" href="https://www.dextools.io/app/uniswap/pair-explorer/0xe46456153cf0e767528f4a4cd3b613d5c4101e48">
+<img src="~/@/assets/dextools.png" alt="" class="middle-4 sibling-icon">
+</a>
+  <a target="_blank" href="https://app.uniswap.org/#/add/0x1e1eed62f8d82ecfd8230b8d283d5b5c1ba81b55/ETH">
+<img src="~/@/assets/liquidity.png" alt="" class="sibling-icon">
+</a>
+</div>
+
+</div>  
+<div class="about-text">
+    <Countdown deadline="June 1, 2022"></Countdown>
+          <h1>
+      </h1>
+    </div>
+
+
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+import JJParallaxCard from 'jj-parallax-card'
+import Countdown from 'vuejs-countdown'
+export default {
+  data() {
+    return {
+      form: {
+        quantity: ''
+      },
+      modalLoginOpen: false,
+      modalMakepotionOpen: false
+    };
+  },
+ components: {
+   Countdown,
+  'jj-parallax-card':JJParallaxCard,
+    
+  },
+};
+</script>
+<style scoped>
+.hasEffect {
+  cursor: pointer;
+}
+</style>
+
